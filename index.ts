c8923b418d716725f5121185e7096c4057aa8ce0
Harshad Sharma <harshad@sharma.io>
2022-09-17 15:59:01 +0530

    Validate settings before executing commands

--- index.ts
+++ index.ts
@@ -26,6 +26,55 @@ const settingsSchema: SettingSchemaDesc[] = [
     },
 ]
 
+const weatherUnits = {
+    default: { temperature: "°K", distance: "m", speed: "m/s", pressure: "hPa" },
+    metric: { temperature: "°C", distance: "m", speed: "m/s", pressure: "hPa" },
+    imperial: { temperature: "°F", distance: "m", speed: "mph", pressure: "hPa" },
+}
+
+function settings_are_valid() {
+    const city = logseq.settings!["openWeatherCity"]
+    const units = logseq.settings!["openWeatherUnits"]
+    const api_key = logseq.settings!["openWeatherAPIKey"]
+
+    if (!api_key) {
+        console.error("Need API key set in settings.")
+        logseq.UI.showMsg(
+            "Need OpenWeather API key. Add one in plugin settings.",
+            "error"
+        )
+        return false
+    }
+
+    if (!city) {
+        console.error("City not configured for OpenWeather plugin.")
+        logseq.UI.showMsg(
+            "Configure your city in plugin settings.",
+            "error"
+        )
+        return false
+    }
+
+    if (!units) {
+        console.error("Units of measurement not configured")
+        logseq.UI.showMsg(
+            "Configure units of measurement in plugin settings.",
+            "error"
+        )
+        return false
+    }
+
+    if (!(units in weatherUnits)) {
+        console.error("Units of measurement misconfigured")
+        logseq.UI.showMsg(
+            "Configure units of measurement in plugin settings.",
+            "error"
+        )
+        return false
+    }
+
+    return true
+}
 
 function main() {
     logseq.useSettingsSchema(settingsSchema);
@@ -33,15 +82,23 @@ function main() {
     logseq.Editor.registerSlashCommand(
         'OpenWeatherDescribe',
         async () => {
+            if (!settings_are_valid()) {
+                return
+            }
             await logseq.Editor.insertAtEditingCursor("Describes current weather in a single line.")
         },
     )
     logseq.Editor.registerSlashCommand(
         'OpenWeatherProperties',
         async () => {
+            if (!settings_are_valid()) {
+                return
+            }
             await logseq.Editor.insertAtEditingCursor("Adds current weather as properties.")
         },
     )
+
+    console.info("logseq-openweather-plugin loaded")
 }
 
 logseq.ready(main).catch(console.error)
